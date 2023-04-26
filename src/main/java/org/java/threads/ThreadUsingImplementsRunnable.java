edb865036b8066d313676c8255a5145d5270f849
Dante Alligheri <shashank996419@gmail.com>
2023-04-26 09:49:46 +0530

    Initial commit with the thread package

---
+++ src/main/java/org/java/threads/ThreadUsingImplementsRunnable.java
@@ -0,0 +1,22 @@
+package org.java.threads;
+
+public class ThreadUsingImplementsRunnable implements Runnable{
+
+    /*Why go for this approach
+    * If we want to implement another interface or extend another classes this approach will allow it
+    *It won't load all the overhead methods */
+
+    // Refer this link to dive into more on the topic: https://javaconceptoftheday.com/extends-thread-vs-implements-runnable-in-java/
+
+    @Override
+    public void run() {
+        // Some code here
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ThreadUsingImplementsRunnable());
+        thread.start();
+
+        // Check out all the other methods as well
+    }
+}
---
+++ src/main/java/org/java/threads/TypesOfThreads.java
@@ -0,0 +1,51 @@
+package org.java.threads;
+
+public class TypesOfThreads extends Thread {
+
+
+    public TypesOfThreads(Runnable runnable) {
+    }
+
+    public TypesOfThreads() {
+    }
+
+    /*There are two types of threads
+     * 1. User threads
+     * 2. Daemon threads*/
+
+    /*
+     * 1. User threads : User threads are threads which are created by the application or user. They are high priority threads.
+     *                   JVM will not exit until all user threads finish their execution. JVM wait for user threads to finish their task.
+     *                   These threads are foreground threads.*/
+
+    /*2. Daemon threads: These are created by the JVM. It runs in the background, to perform some tasks like Garbage Collection.
+    *                    Less priority threads. JVM will not wait for these threads to complete their tasks. It'll exit as soon as 
+    *                    user threads are executed. */
+    @Override
+    public void run() {
+        // Some functional code here
+    }
+
+    public static void main(String[] args) {
+
+        TypesOfThreads thread = new TypesOfThreads();
+        thread.start();
+        System.out.println("thread.isDaemon() = " + thread.isDaemon()); // Output: false
+
+        thread.setDaemon(true);
+        System.out.println("thread.isDaemon() after setting the thread to daemon explicitly = " + thread.isDaemon()); // Output: true
+
+        TypesOfThreads threads = new TypesOfThreads(new Runnable(){
+
+            @Override
+            public void run() {
+                System.out.println("From anonymous inner class");
+            }
+        });
+
+        threads.start();
+
+
+
+    }
+}
