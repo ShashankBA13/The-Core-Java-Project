8b26ddcb0e89cbe4d6137aeec6fc00a0db5e0ad3
Dante Alligheri <shashank996419@gmail.com>
2023-04-29 18:33:36 +0530

    committing two new file of depciting diff topics in threads

---
+++ src/main/java/org/java/threads/IllegalThreadStateException.java
@@ -0,0 +1,37 @@
+package org.java.threads;
+
+public class IllegalThreadStateException extends Thread{
+
+    public void run() {
+        try {
+            System.out.println("Hello from try block of run method");
+            Thread.sleep(1000);
+        } catch (Exception  e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String...  args) {
+        IllegalThreadStateException illegalThreadStateException = new IllegalThreadStateException();
+        Thread t = new Thread(illegalThreadStateException);
+
+        /* This is One way to create the exception
+        t.start();
+        t.start(); // Throws illegal thread state exception, because the thread is already running.
+         */
+
+        t.start();
+        try {
+            System.out.println("Inside the main method try block");
+            Thread.sleep(3000);
+            System.out.println("Main thread is active");
+        }catch (Exception e) {
+            e.getMessage();
+        }
+
+        // Throws an illegalThreadStateException because the main thread is already Dead.
+        t.start();
+        System.out.println("This won't be printed");
+    }
+
+}
---
+++ src/main/java/org/java/threads/LifeCycleOfThread.java
@@ -0,0 +1,46 @@
+package org.java.threads;
+
+public class LifeCycleOfThread extends Thread{
+
+    // Thread has the following lifecycle
+    /*
+    1.  New State
+    2. Active State
+    3. Waiting/Blocked State
+    4. Timed Waiting State
+    5. Terminated State
+     */
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new MyRunnable());
+
+        //start the thread
+        thread.start();
+
+        //wait for the thread to finish tasks
+        thread.join();
+
+        //statement printing the program is complete
+        System.out.println("Program finished");
+
+    }
+
+   private static class MyRunnable implements Runnable {
+
+       @Override
+       public void run() {
+           //print a message to indicate that the thread has started
+           System.out.println("Thread is running");
+
+           //Giving some work
+           try {
+               Thread.sleep(1000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+
+           //print a message to indicate that the thread has finished
+           System.out.println("Thread is finished");
+       }
+   }
+}
