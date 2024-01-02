e1d53b428bb25ec18cdbd095c0d05c31a0b0d9c8
Dante Alligheri <Shashank996419@gmail.com>
2024-01-02 11:50:32 +0530

    OOPs: Polymorphism changes

--- src/main/java/org/java/oop/polymorphism/runtime/TestRuntimePolymorphism.java
+++ src/main/java/org/java/oop/polymorphism/runtime/TestRuntimePolymorphism.java
@@ -3,9 +3,11 @@ package org.java.oop.polymorphism.runtime;
 public class TestRuntimePolymorphism {
 
     public static void main(String[] args) {
+        Animal animal = new Animal();
         Animal dog = new Dog();
         Animal cat = new Cat();
 
+        animal.makeSound();
         dog.makeSound();
         cat.makeSound();
     }
